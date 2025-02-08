6749eed301f1bc8c005d35935502200207fe4ea5
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2025-02-08 14:00:01 -0300

    Create config.py
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ SysAdmin-Tools/ActiveDirectory-SSO-Integrations/Python-API/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    # Generalized LDAP configuration values
+    LDAP_SERVER = "ldap://ldap.example.com:3268"
+    LDAP_BASE_DN = "dc=HEADQ,dc=SCRIPTGUY"
+    LDAP_BIND_USER = "binduser@scriptguy"
+    LDAP_BIND_PASSWORD = os.environ.get("LDAP_PASSWORD", "your_generic_password")
+    LDAP_USER_SEARCH_FILTER = "(sAMAccountName={username})"
+    LDAP_GROUP_SEARCH_FILTER = "(member={user_dn})"
