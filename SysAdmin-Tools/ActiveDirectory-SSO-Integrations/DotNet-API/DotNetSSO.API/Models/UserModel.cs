d09e612cacbb8393294a06e04185cf2ea34bd896
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2025-02-16 14:03:18 -0300

    Create UserModel.cs
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ SysAdmin-Tools/ActiveDirectory-SSO-Integrations/DotNet-API/DotNetSSO.API/Models/UserModel.cs
@@ -0,0 +1,11 @@
+namespace DotNetSSO.API.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+    }
+}
