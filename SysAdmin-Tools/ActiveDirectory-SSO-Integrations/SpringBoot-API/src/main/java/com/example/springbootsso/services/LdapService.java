01602f4d54cbe93e8d6ff3cb4503e77ab8e6e88a
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2025-02-16 16:15:58 -0300

    Create LdapService.java
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ SysAdmin-Tools/ActiveDirectory-SSO-Integrations/SpringBoot-API/src/main/java/com/example/springbootsso/services/LdapService.java
@@ -0,0 +1,11 @@
+package com.example.springbootsso.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LdapService {
+
+    public boolean authenticate(String username, String password) {
+        return "admin".equals(username) && "password".equals(password);
+    }
+}
