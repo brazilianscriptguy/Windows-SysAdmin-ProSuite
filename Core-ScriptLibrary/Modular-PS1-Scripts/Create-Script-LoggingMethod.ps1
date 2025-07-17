08f2a5ea90a2fb2799ccd29da821c07d4cc2c951
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2025-07-17 17:52:04 -0300

    Update Create-Script-LoggingMethod.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Core-ScriptLibrary/Modular-PS1-Scripts/Create-Script-LoggingMethod.ps1
+++ Core-ScriptLibrary/Modular-PS1-Scripts/Create-Script-LoggingMethod.ps1
@@ -4,7 +4,7 @@ function Write-Log {
         [Parameter(Mandatory = $true)]
         [string]$Message,
         [Parameter(Mandatory = $false)]
-        [ValidateSet("INFO", "WARNING", "ERROR")]
+        [ValidateSet("INFO", "WARNING", "ERROR", "DEBUG")]
         [string]$Level = "INFO",
         [Parameter(Mandatory = $false)]
         [string]$LogDirectory = (Join-Path $env:LOCALAPPDATA "ScriptLogs")
@@ -20,10 +20,11 @@ function Write-Log {
 
     $logEntry = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') [$Level] $Message"
     Add-Content -Path $LogFile -Value $logEntry -ErrorAction Stop
-    if ($Level -eq "ERROR") {
-        Write-Error $logEntry
-    } elseif ($VerbosePreference -eq 'Continue') {
-        Write-Verbose $logEntry
+    switch ($Level) {
+        "ERROR" { Write-Error $logEntry }
+        "WARNING" { Write-Warning $logEntry }
+        "DEBUG" { if ($VerbosePreference -eq 'Continue' -or $PSCmdlet.MyInvocation.BoundParameters["Verbose"]) { Write-Verbose $logEntry } }
+        "INFO" { if ($VerbosePreference -eq 'Continue') { Write-Verbose $logEntry } }
     }
 }
 
@@ -31,4 +32,5 @@ function Write-Log {
 $VerbosePreference = 'Continue'
 Write-Log "Script execution started" -LogDirectory (Join-Path $env:LOCALAPPDATA "CustomLogs")
 Write-Log "Processing task" -Level "INFO"
+Write-Log "Debugging variable: $variable" -Level "DEBUG"
 Write-Log "An error occurred" -Level "ERROR"
