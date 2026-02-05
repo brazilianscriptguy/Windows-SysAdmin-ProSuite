7e2e226f40d9edf811f216a83089dd834a8b8a9b
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2026-02-05 12:04:25 -0300

    Update EventID-Count-AllEvtx-Events.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- BlueTeam-Tools/EventLogMonitoring/EventID-Count-AllEvtx-Events.ps1
+++ BlueTeam-Tools/EventLogMonitoring/EventID-Count-AllEvtx-Events.ps1
@@ -2,7 +2,6 @@
 .SYNOPSIS
     PowerShell Script for Counting EventID Occurrences in EVTX Files using Log Parser.
     
-
 .DESCRIPTION
     This script counts occurrences of each EventID in EVTX files within a selected folder using 
     COM-based LogQuery, exporting the results to a CSV file with user-configurable paths via a GUI.
