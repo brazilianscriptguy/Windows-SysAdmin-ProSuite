f5b58a0ce7ba44bb005dd3795d2005ee3d666409
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2026-02-19 08:01:16 -0300

    Update wsus-verify-fragmentation.sql
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- SysAdmin-Tools/WSUS-Management-Tools/Scripts/wsus-verify-fragmentation.sql
+++ SysAdmin-Tools/WSUS-Management-Tools/Scripts/wsus-verify-fragmentation.sql
@@ -1,44 +1,30 @@
-USE [SUSDB];
-GO
+SET NOCOUNT ON;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_PADDING ON;
+SET ANSI_WARNINGS ON;
+SET CONCAT_NULL_YIELDS_NULL ON;
+SET ARITHABORT ON;
+SET NUMERIC_ROUNDABORT OFF;
 
-DECLARE @MinPages   int   = 100;
-DECLARE @RebuildPct float = 30.0;
-DECLARE @ReorgPct   float = 5.0;
-
+SET NOCOUNT ON;
 SELECT
-    s.[name]  AS SchemaName,
-    t.[name]  AS TableName,
-    i.[name]  AS IndexName,
-    i.type_desc AS IndexType,
-    ips.partition_number AS PartitionNumber,
-    ips.page_count       AS PageCount,
-    ROUND(CAST(ips.avg_fragmentation_in_percent AS decimal(9,4)), 2) AS FragmentationPercent,
-    CASE
-        WHEN ips.avg_fragmentation_in_percent >= @RebuildPct THEN 'High'
-        WHEN ips.avg_fragmentation_in_percent >= @ReorgPct   THEN 'Medium'
-        ELSE 'Low'
-    END AS FragmentationLevel,
-    CASE
-        WHEN ips.page_count < @MinPages THEN 'NONE (small index)'
-        WHEN ips.avg_fragmentation_in_percent >= @RebuildPct THEN 'REBUILD'
-        WHEN ips.avg_fragmentation_in_percent >= @ReorgPct   THEN 'REORGANIZE'
-        ELSE 'NONE'
-    END AS MaintenanceRecommendation
-FROM sys.dm_db_index_physical_stats(DB_ID('SUSDB'), NULL, NULL, NULL, 'LIMITED') AS ips
-JOIN sys.indexes AS i
-  ON ips.[object_id] = i.[object_id]
- AND ips.index_id    = i.index_id
-JOIN sys.tables AS t
-  ON i.[object_id] = t.[object_id]
-JOIN sys.schemas AS s
-  ON t.[schema_id] = s.[schema_id]
-WHERE
-    i.[name] IS NOT NULL
-    AND i.is_disabled = 0
-    AND i.is_hypothetical = 0
-    AND i.index_id > 0
-    AND i.type IN (1,2)
-    AND ips.page_count >= @MinPages
-ORDER BY
-    FragmentationPercent DESC,
-    PageCount DESC;
+  DB_NAME() AS [DatabaseName],
+  OBJECT_SCHEMA_NAME(ips.object_id) AS [SchemaName],
+  OBJECT_NAME(ips.object_id) AS [TableName],
+  i.name AS [IndexName],
+  ips.index_id AS [IndexId],
+  ips.page_count AS [PageCount],
+  ips.avg_fragmentation_in_percent AS [FragPct],
+  CASE
+    WHEN ips.page_count < 1000 THEN 'SKIP (small index)'
+    WHEN ips.avg_fragmentation_in_percent >= 30 THEN 'REBUILD'
+    WHEN ips.avg_fragmentation_in_percent >= 10 THEN 'REORGANIZE'
+    ELSE 'OK'
+  END AS [Recommendation]
+FROM sys.dm_db_index_physical_stats(DB_ID('SUSDB'), NULL, NULL, NULL, 'LIMITED') ips
+JOIN sys.indexes i
+  ON ips.object_id = i.object_id AND ips.index_id = i.index_id
+WHERE ips.index_id > 0
+  AND i.is_disabled = 0
+ORDER BY ips.avg_fragmentation_in_percent DESC, ips.page_count DESC;
