a34d6d3cd9fcff2e0e65cce35ad9c956a4dfc0cc
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2026-02-19 08:14:47 -0300

    Update Check-WSUS-AdminAssembly.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- SysAdmin-Tools/WSUS-Management-Tools/Check-WSUS-AdminAssembly.ps1
+++ SysAdmin-Tools/WSUS-Management-Tools/Check-WSUS-AdminAssembly.ps1
@@ -5,7 +5,7 @@
 .DESCRIPTION
     Validates that the WSUS Administration API can be loaded and (optionally) that a WSUS server
     can be reached via AdminProxy / Get-WsusServer. Designed to be used standalone or as a preflight
-    dependency check by the WSUS Maintenance GUI.
+    dependency check by the WSUS Maintenance GUI. 
 
     Hardened:
       - StrictMode safe (param at top)
